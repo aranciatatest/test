6ce4836cb62456a7923c01a630cb579eebdccb68
mick <stedelucia@gmail.com>
2023-12-30 03:09:25 +0100

    rimosse tutte le classi aggiuntive in Main.java e aggiunta la classe somma

--- Main.java
+++ Main.java
@@ -1,21 +1,8 @@
 public class Main{
     public static void main(String[] args){
-        int buonaserraaaaa;
-        char buonasera;
-
-        hahahaha
-    }
+       
 }
 
-class test10{
-    public static void function(){
-        
-    }
-}
-class test11{
-testestest
-dfsffsf
-}
-class test12{
-    sffsfsf
+class somma{
+    
 }
